41a422a81d0c13a1b009acbda347efb18d7e06c0
Thanh Tung <thanhtung28400@gmail.com>
2022-04-24 22:37:14 +0700

    feat: create a top navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import AccountMenu from "./AccountMenu";
+import learnIcon from "../icons/book_32.png";
+import focusLearnIcon from "../icons/book_color_32.png";
+import dictIcon from "../icons/dict_32.png";
+import focusDictIcon2 from "../icons/dict_color_32.png";
+
+const myBlue = "#1cb0f6";
+const myGray = "#afafaf";
+
+function Navbar() {
+  const [page, setPage] = useState("learn");
+
+  return (
+    <nav>
+      <span className="nav-logo">Fakelingo</span>
+
+      <div className="nav-link">
+        <Link to="/learn" onClick={() => setPage("learn")}>
+          <img src={page === "learn" ? focusLearnIcon : learnIcon} alt="" />
+          &nbsp;&nbsp;
+          <span style={{ color: page === "learn" ? myBlue : myGray }}>Học</span>
+        </Link>
+        
+        <Link to="/dictionary" onClick={() => setPage("dictionary")}>
+          <img src={page === "dictionary" ? focusDictIcon2 : dictIcon} alt="" />
+          &nbsp;&nbsp;
+          <span style={{ color: page === "dictionary" ? myBlue : myGray }}>
+            Từ điển
+          </span>
+        </Link>
+      </div>
+
+      <div className="nav-account">
+        <AccountMenu />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
